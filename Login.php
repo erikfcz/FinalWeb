6a4b1a7007ae4a1b351e4f01c9b88841aad66e32
erikfcz <72215578+erikfcz@users.noreply.github.com>
2021-12-05 17:14:13 -0600

    Update Login.php

--- Login.php
+++ Login.php
@@ -5,12 +5,11 @@
     <meta charset="UTF-8">
     <title></title> 
     <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0, minimum-scale=1.0">
-                <link rel="stylesheet" href="CSS/all.css" >
-                <link rel="stylesheet" href="CSS/StyleMiCuenta.css" type="text/css"
+                <link rel="stylesheet" href="all.css" >
+                <link rel="stylesheet" href="StyleMiCuenta.css" type="text/css"
 </head> 
  
 <body>
-
 <header>
  <class="header">
      <div class="container">
@@ -19,14 +18,14 @@
     </div>
 
   <nav class="menu">
-    <a href="MENUCHIDO.HTML">Inicio</a>
-    <a href="QS.HTML">Quienes somos</a>
-    <a href="Horarios.HTML">Horario</a>
-    <a href="Contacto.HTML">Contactanos</a>
+    <a href="index.html">Inicio</a>
+    <a href="QS.html">Quienes somos</a>
+    <a href="Horarios.html">Horario</a>
+    <a href="Contacto.html">Contactanos</a>
      </nav>
 </div>
 
-<body background="imagenesInicio/Pan de Temporada Fondo.jpg">
+<body background="Pan de Temporada Fondo.jpg">
 <h2 style="text-align:center;">¡APROVECHA NUESTRAS PROMOCIONES Y DESCUENTOS!</h2>
 </header>
 
@@ -36,7 +35,7 @@
      <div class="cont-menu">
         <nav>
             <a href="http://localhost:8080/ss/public/WEB/Login.php">Mi cuenta</a>
-            <a href="MENUCHIDO.HTML">Inicio</a>
+            <a href="index.html">Inicio</a>
             <a href="https://www.google.com/maps/place/Manhattan/@3.0393877,-120.4945631,3z/data=!4m9!1m2!2m1!1spanaderia+manhattan!3m5!1s0x85cfd9416c7d7b81:0xe2afd5d6f8eeed6a!8m2!3d19.3064056!4d-98.2394482!15sChNwYW5hZGVyaWEgbWFuaGF0dGFuWhUiE3BhbmFkZXJpYSBtYW5oYXR0YW6SAQVzdG9yZQ">Sucursales</a>
             <a href="https://www.facebook.com/pages/category/Bakery/Manhattan-Panader%C3%ADa-159615300719787/">Facebook</a>
             <a href="https://www.instagram.com/manhattanpanaderia/?hl=es">Instagram</a>
@@ -46,25 +45,16 @@
        </div>
 </div>
 
-
 <div id="general">
     <div id="head">
     </div>
 <h1 class="title">¡TENEMOS ALGO PARA CONCENTIRTE! </h1>
     
-
-
-
-
-
-
 <form class="formulario" method="post">
     
     <h1>Login</h1>
      <div class="contenedor">
-     
-     
-         
+              
          <div class="input-contenedor">
          <i class="fas fa-envelope icon"></i>
          <input type="text" name="correo" placeholder="Correo Electronico">
@@ -78,7 +68,7 @@
          </div>
          <input type="submit" name="Login" value="Login" class="button">
          <p>Al registrarte, aceptas nuestras Condiciones de uso y Política de privacidad.</p>
-         <p>¿No tienes una cuenta? <a class="link" href="Mi Cuenta Registro.HTML">Registrate </a></p>
+         <p>¿No tienes una cuenta? <a class="link" href="Mi Cuenta Registro.html">Registrate </a></p>
          <?php
             include('login2.php');
          ?>
