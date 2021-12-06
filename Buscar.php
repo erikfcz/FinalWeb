a4b0d7683d33ebc21f0141ea4d4d5de3347f6b51
erikfcz <72215578+erikfcz@users.noreply.github.com>
2021-12-05 19:50:44 -0600

    Add files via upload

---
+++ Buscar.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Realizar pedido</title> 
+    <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0, minimum-scale=1.0">
+                <link rel="stylesheet" href="CSS/all.css" >
+     <link rel="stylesheet" href="CSS/StyleMiCuenta.css" type="text/css"
+</head> 
+ 
+<body>
+
+<header>
+ <class="header">
+     <div class="container">
+     <div class="btn-menu">
+          <label for="btn-menu">☰</label>
+    </div>
+
+  <nav class="menu">
+    <a href="MENUCHIDO.HTML">Inicio</a>
+    <a href="QS.HTML">Quienes somos</a>
+    <a href="Horarios.HTML">Horario</a>
+    <a href="Contacto.HTML">Contactanos</a>
+     </nav>
+</div>
+
+<body background="imagenesInicio/Pan de Temporada Fondo.jpg">
+<h2 style="text-align:center;">¡APROVECHA NUESTRAS PROMOCIONES Y DESCUENTOS!</h2>
+</header>
+
+<div class="capa"></div>
+<input type="checkbox" id="btn-menu">
+<div class="container-menu">
+     <div class="cont-menu">
+        <nav>
+            <a href="Mi Cuenta Login.HTML">Mi cuenta</a>
+            <a href="MENUCHIDO.HTML">Inicio</a>
+            <a href="https://www.google.com/maps/place/Manhattan/@3.0393877,-120.4945631,3z/data=!4m9!1m2!2m1!1spanaderia+manhattan!3m5!1s0x85cfd9416c7d7b81:0xe2afd5d6f8eeed6a!8m2!3d19.3064056!4d-98.2394482!15sChNwYW5hZGVyaWEgbWFuaGF0dGFuWhUiE3BhbmFkZXJpYSBtYW5oYXR0YW6SAQVzdG9yZQ">Sucursales</a>
+            <a href="https://www.facebook.com/pages/category/Bakery/Manhattan-Panader%C3%ADa-159615300719787/">Facebook</a>
+            <a href="https://www.instagram.com/manhattanpanaderia/?hl=es">Instagram</a>
+            <a href="https://www.youtube.com/channel/UC8El-KamqNxKQg-r15BmIxw">Youtube</a>
+        </nav>
+<label for="btn-menu">✖️</label>
+       </div>
+</div>
+
+
+<div id="general">
+    <div id="head">
+    </div>
+<h1 class="title">¡QUEREMOS CONOCERTE! </h1>
+    
+
+ <form class="formulario" method="post">
+    
+    <h1>Buscar un pedido</h1>
+     <div class="contenedor">
+
+        <div class="input-contenedor">
+         <i class="fas fa-user icon"></i>
+         <select type="text" name="sel_pastel" >
+             <option type="text" name="sel_pastel" class="opciones">Seleccione el Pastel</option>
+             <option type="text" name="sel_pastel" class="opciones">(MP) Pastel tres leches de chocolate con dulce de leche</option>
+             <option type="text" name="sel_pastel" class="opciones">(MP) Chocoflan, pastel imposible</option>
+             <option type="text" name="sel_pastel" class="opciones">(MP) El pastel de mil hojas</option>
+             <option type="text" name="sel_pastel" class="opciones">(PBa) Pastel de la santa cruz</option>
+             <option type="text" name="sel_pastel" class="opciones">(PBa) Pastel a la crepa</option>
+             <option type="text" name="sel_pastel" class="opciones">(PBa) Pastel gracias a Dios</option>
+             <option type="text" name="sel_pastel" class="opciones">(PBo) Couple Cake</option>
+             <option type="text" name="sel_pastel" class="opciones">(PBo) Couple Cake</option>
+             <option type="text" name="sel_pastel" class="opciones">(PBo) El pastel de la pasion</option>
+         </select>
+         </div>
+
+         <div class="input-contenedor">
+         <i class="fas fa-user icon"></i>
+         <select type="text" name="dia" >
+             <option type="text" name="dia" class="opciones">Dia de entrega</option>
+             <option type="text" name="dia" class="opciones">1</option>
+             <option type="text" name="dia" class="opciones">2</option>
+             <option type="text" name="dia" class="opciones">3</option>
+             <option type="text" name="dia" class="opciones">4</option>
+             <option type="text" name="dia" class="opciones">5</option>
+             <option type="text" name="dia" class="opciones">6</option>
+             <option type="text" name="dia" class="opciones">7</option>
+             <option type="text" name="dia" class="opciones">8</option>
+             <option type="text" name="dia" class="opciones">9</option>
+             <option type="text" name="dia" class="opciones">10</option>
+             <option type="text" name="dia" class="opciones">11</option>
+             <option type="text" name="dia" class="opciones">12</option>
+             <option type="text" name="dia" class="opciones">13</option>
+             <option type="text" name="dia" class="opciones">14</option>
+             <option type="text" name="dia" class="opciones">15</option>
+             <option type="text" name="dia" class="opciones">16</option>
+             <option type="text" name="dia" class="opciones">17</option>
+             <option type="text" name="dia" class="opciones">18</option>
+             <option type="text" name="dia" class="opciones">19</option>
+             <option type="text" name="dia" class="opciones">20</option>
+             <option type="text" name="dia" class="opciones">21</option>
+             <option type="text" name="dia" class="opciones">22</option>
+             <option type="text" name="dia" class="opciones">23</option>
+             <option type="text" name="dia" class="opciones">24</option>
+             <option type="text" name="dia" class="opciones">25</option>
+             <option type="text" name="dia" class="opciones">26</option>
+             <option type="text" name="dia" class="opciones">27</option>
+             <option type="text" name="dia" class="opciones">28</option>
+             <option type="text" name="dia" class="opciones">29</option>
+             <option type="text" name="dia" class="opciones">30</option>
+             <option type="text" name="dia" class="opciones">31</option>
+         </select>
+         </div>
+
+         <div class="input-contenedor">
+         <i class="fas fa-user icon"></i>
+         <select type="text" name="mes" >
+             <option type="text" name="mes" class="opciones">Mes de entrega</option>
+             <option type="text" name="mes" class="opciones">Enero</option>
+             <option type="text" name="mes" class="opciones">Febrero</option>
+             <option type="text" name="mes" class="opciones">Marzo</option>
+             <option type="text" name="mes" class="opciones">Abril</option>
+             <option type="text" name="mes" class="opciones">Mayo</option>
+             <option type="text" name="mes" class="opciones">Junio</option>
+             <option type="text" name="mes" class="opciones">Julio</option>
+             <option type="text" name="mes" class="opciones">Agosto</option>
+             <option type="text" name="mes" class="opciones">Septiembre</option>
+             <option type="text" name="mes" class="opciones">Octubre</option>
+             <option type="text" name="mes" class="opciones">Noviembre</option>
+             <option type="text" name="mes" class="opciones">Diciembre</option>
+         </select>
+         </div>
+         
+
+         <div class="input-contenedor">
+         <i class="fas fa-user icon"></i>
+         <select type="text" name="ano" >
+             <option type="text" name="ano" class="opciones">Año de entrega</option>
+             <option type="text" name="ano" class="opciones">2021</option>
+             <option type="text" name="ano" class="opciones">2022</option>
+             <option type="text" name="ano" class="opciones">2023</option>
+         </select>
+         </div>
+
+         <div class="input-contenedor">
+         <i class="fas fa-user icon"></i>
+         <select type="text" name="hora" >
+             <option type="text" name="hora" class="opciones">Hora de entrega</option>
+             <option type="text" name="hora" class="opciones">07:00</option>
+             <option type="text" name="hora" class="opciones">08:00</option>
+             <option type="text" name="hora" class="opciones">09:00</option>
+             <option type="text" name="hora" class="opciones">10:00</option>
+             <option type="text" name="hora" class="opciones">11:00</option>
+             <option type="text" name="hora" class="opciones">12:00</option>
+             <option type="text" name="hora" class="opciones">13:00</option>
+             <option type="text" name="hora" class="opciones">14:00</option>
+             <option type="text" name="hora" class="opciones">15:00</option>
+             <option type="text" name="hora" class="opciones">16:00</option>
+             <option type="text" name="hora" class="opciones">17:00</option>
+             <option type="text" name="hora" class="opciones">18:00</option>
+             <option type="text" name="hora" class="opciones">19:00</option>
+             <option type="text" name="hora" class="opciones">20:00</option>
+             <option type="text" name="hora" class="opciones">21:00</option>
+             <option type="text" name="hora" class="opciones">22:00</option>
+             <option type="text" name="hora" class="opciones">23:00</option>
+         </select>
+         </div>
+
+         <div class="input-contenedor">
+        <i class="fas fa-key icon"></i>
+         <input type="text" name="CP" placeholder="Ingrese el Código Postal">
+         </div>
+
+         <input type="submit" value="Buscarar Pedido" name="BuscarP" class="button">
+         <p>Al registrar el pedido, acepta que todos los datos ingresados son correctos. NO hay modificaciones ni devoluciones. </p>
+         <p>Si desea cancelar el pedido vaya al apartado de cancelaciones. 
+
+        
+     </div>
+    </form>
+    <div class="tabla">
+              <?php
+            include('busqueda.php');
+         ?>
+    </div>
+ </div>
+
+ <style type="text/css">
+     select{
+        margin:0;
+        padding: 0;
+        width: 100%; 
+        height: 40px;
+     }
+     option{
+        font-size: 17px;
+        color: brown;
+     }
+
+     input{
+        width: 100%;
+     }
+
+     .tabla{
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 200px;
+        background-color: brown;
+        text-align: center;
+     }
+
+     table{
+        margin: 0;
+        color: white;
+        text-align: center;
+        font-size: 10px;
+     }
+ </style>
+</body>
+</html>
